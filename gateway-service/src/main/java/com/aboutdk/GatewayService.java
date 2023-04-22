a1ccb0b9c2f5a3ac61d9403befc66f2d1c65fb8f
zhouyang <602482790@qq.com>
2023-04-22 23:45:05 +0800

    v0.1.1 modify GatewayService

--- gateway-service/src/main/java/com/aboutdk/GatewayService.java
+++ gateway-service/src/main/java/com/aboutdk/GatewayService.java
@@ -1,11 +1,17 @@
 package com.aboutdk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,16 +20,45 @@ public class GatewayService {
       SpringApplication.run(GatewayService.class, args);
    }
 
+   @Autowired
+   private DiscoveryClient discoveryClient;
+
    @Bean
    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 
+      List<String> services = this.discoveryClient.getServices();
+      List<ServiceInstance> instances = new ArrayList<ServiceInstance>();
+      services.forEach(serviceName -> {
+         this.discoveryClient.getInstances(serviceName).forEach(instance ->{
+            instances.add(instance);
+         });
+      });
+
+      String noteServiceIp = "";
+      String userServiceIp = "";
+      for (int i = 0; i < instances.size(); i++) {
+         ServiceInstance serviceInstance = instances.get(i);
+
+         String instanceId = serviceInstance.getInstanceId();
+         String[] arr = instanceId.split(":");
+
+         if ("NoteApplication".equalsIgnoreCase(arr[1])) {
+            noteServiceIp = "http://" + arr[0] + ":" + arr[2];
+         }
+         if ("UserApplication".equalsIgnoreCase(arr[1])) {
+            userServiceIp = "http://" + arr[0] + ":" + arr[2];
+         }
+      }
+
+      String finalNoteServiceIp = noteServiceIp;
+      String finalUserServiceIp = userServiceIp;
       return builder.routes()
               .route(p -> p
-                      .path("/api/register/**")
-                      .uri("http://localhost:8081"))
-              .route(p -> p
                       .path("/api/note/**")
-                      .uri("http://localhost:8080"))
+                      .uri(finalNoteServiceIp))
+              .route(p -> p
+                      .path("/api/register/**")
+                      .uri(finalUserServiceIp))
               .route("pc",
                       r -> r.path("/pc")
                               .filters(f -> f.rewritePath("/pc", "/pc/index.html"))
