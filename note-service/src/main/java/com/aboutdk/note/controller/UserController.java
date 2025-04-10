aa29e5f62d2fe4c858330f51417c68741f086065
zhouyang <602482790@qq.com>
2025-04-10 16:17:51 +0800

    v 25.04.10 更改数据库为阿里云mysql 发布成功

--- note-service/src/main/java/com/aboutdk/note/controller/UserController.java
+++ note-service/src/main/java/com/aboutdk/note/controller/UserController.java
@@ -33,136 +33,136 @@ import java.util.stream.Collectors;
 @Slf4j
 public class UserController {
 
-   @Autowired
-   private IUserService userService;
+    @Autowired
+    private IUserService userService;
 
-   @Autowired
-   private NoteServiceImpl noteService;
+    @Autowired
+    private NoteServiceImpl noteService;
 
-   @Autowired
-   private TokenService tokenService;
+    @Autowired
+    private TokenService tokenService;
 
-   @Autowired
-   private UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
-   @PostMapping("/register")
-   private ResponseVO<UserDO> register(@Valid @RequestBody RegisterUserForm form, BindingResult bindingResult) {
-      if (bindingResult.hasErrors()) {
-         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-         log.error("error field is : {} ,message is : {}", fieldErrors.get(0).getField(), fieldErrors.get(0).getDefaultMessage());
-         return ResponseVO.error(ResponseEnum.ERROR, fieldErrors.get(0).getField() + " " + fieldErrors.get(0).getDefaultMessage());
-      }
+    @PostMapping("/register")
+    private ResponseVO<UserDO> register(@Valid @RequestBody RegisterUserForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            log.error("error field is : {} ,message is : {}", fieldErrors.get(0).getField(), fieldErrors.get(0).getDefaultMessage());
+            return ResponseVO.error(ResponseEnum.ERROR, fieldErrors.get(0).getField() + " " + fieldErrors.get(0).getDefaultMessage());
+        }
 
-      if (form.getUsername().trim().contains(" ")) {
-         return ResponseVO.error(ResponseEnum.ERROR, "username must not contain space");
-      }
-      if (form.getPassword().trim().contains(" ")) {
-         return ResponseVO.error(ResponseEnum.ERROR, "password must not contain space");
-      }
+        if (form.getUsername().trim().contains(" ")) {
+            return ResponseVO.error(ResponseEnum.ERROR, "username must not contain space");
+        }
+        if (form.getPassword().trim().contains(" ")) {
+            return ResponseVO.error(ResponseEnum.ERROR, "password must not contain space");
+        }
 
-      UserDO userDO = userService.register(form);
-      return ResponseVO.success(userDO);
-   }
+        UserDO userDO = userService.register(form);
+        return ResponseVO.success(userDO);
+    }
 
-   @PostMapping("/login")
-   private ResponseVO<String> login(@Valid @RequestBody LoginForm form, BindingResult bindingResult) throws Exception {
-      log.info("request: /user/login");
-      log.info(form.toString());
-      if (bindingResult.hasErrors()) {
-         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-         log.error("error field is : {} ,message is : {}", fieldErrors.get(0).getField(), fieldErrors.get(0).getDefaultMessage());
-         return ResponseVO.error(ResponseEnum.ERROR, fieldErrors.get(0).getField() + " " + fieldErrors.get(0).getDefaultMessage());
-      }
+    @PostMapping("/login")
+    private ResponseVO<String> login(@Valid @RequestBody LoginForm form, BindingResult bindingResult) throws Exception {
+        log.info("request: /user/login");
+        log.info(form.toString());
+        if (bindingResult.hasErrors()) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            log.error("error field is : {} ,message is : {}", fieldErrors.get(0).getField(), fieldErrors.get(0).getDefaultMessage());
+            return ResponseVO.error(ResponseEnum.ERROR, fieldErrors.get(0).getField() + " " + fieldErrors.get(0).getDefaultMessage());
+        }
 
-      String username = form.getUsername();
-      String password = form.getPassword();
-      if (username.contains(" ")) {
-         return ResponseVO.error(ResponseEnum.ERROR, "username must not contain space");
-      }
-      if (password.contains(" ")) {
-         return ResponseVO.error(ResponseEnum.ERROR, "password must not contain space");
-      }
+        String username = form.getUsername();
+        String password = form.getPassword();
+        if (username.contains(" ")) {
+            return ResponseVO.error(ResponseEnum.ERROR, "username must not contain space");
+        }
+        if (password.contains(" ")) {
+            return ResponseVO.error(ResponseEnum.ERROR, "password must not contain space");
+        }
 
-      String token = userService.login(username, password);
-      return ResponseVO.success(token);
-   }
+        String token = userService.login(username, password);
+        return ResponseVO.success(token);
+    }
 
-   @GetMapping("/{username}/profile")
-   private ResponseVO<UserVO> getUserProfile(@PathVariable String username) {
-      UserVO userVO = new UserVO();
-      UserDO userDO = userService.findUserDO(username);
-      BeanUtils.copyProperties(userDO, userVO);
+    @GetMapping("/{username}/profile")
+    private ResponseVO<UserVO> getUserProfile(@PathVariable String username) {
+        UserVO userVO = new UserVO();
+        UserDO userDO = userService.findUserDO(username);
+        BeanUtils.copyProperties(userDO, userVO);
 
-      if ("".equals(userDO.getNotePassword())) {
-         userVO.setHasNotePassword(false);
-      } else {
-         userVO.setHasNotePassword(true);
-      }
+        if ("".equals(userDO.getNotePassword())) {
+            userVO.setHasNotePassword(false);
+        } else {
+            userVO.setHasNotePassword(true);
+        }
 
-      return ResponseVO.success(userVO);
-   }
+        return ResponseVO.success(userVO);
+    }
 
-   @PutMapping("/profile")
-   private ResponseVO<UserVO> updateProfile(@RequestBody UserDO userDO) {
-      UserDO newUserDO = userService.updateProfile(userDO);
+    @PutMapping("/profile")
+    private ResponseVO<UserVO> updateProfile(@RequestBody UserDO userDO) {
+        UserDO newUserDO = userService.updateProfile(userDO);
 
-      String username = newUserDO.getUsername();
-      ResponseVO<UserVO> userProfile = this.getUserProfile(username);
-      return userProfile;
-   }
+        String username = newUserDO.getUsername();
+        ResponseVO<UserVO> userProfile = this.getUserProfile(username);
+        return userProfile;
+    }
 
-   @PostMapping("/setNotePassword/{notePassword}")
-   public ResponseVO setNotePassword(@PathVariable String notePassword, @RequestHeader HttpHeaders headers) {
-      String curUser = tokenService.getUsername(headers.getFirst("token"));
+    @PostMapping("/setNotePassword/{notePassword}")
+    public ResponseVO setNotePassword(@PathVariable String notePassword, @RequestHeader HttpHeaders headers) {
+        String curUser = tokenService.getUsername(headers.getFirst("token"));
 
-      UserDO userDO = userService.findUserDO(curUser);
-      if ("null".equals(notePassword)) {
-         // cancel not password
-         userDO.setNotePassword("");
+        UserDO userDO = userService.findUserDO(curUser);
+        if ("null".equals(notePassword)) {
+            // cancel not password
+            userDO.setNotePassword("");
 
-         // set all note encrypt to false
-         List<NoteForm> noteFormList = noteService.findAllByUsername(curUser)
-                 .stream()
-                 .map((noteDO) -> {
-                    NoteForm noteForm = NoteDOMapper.INSTANCE.noteDOToNoteForm(noteDO);
-                    noteForm.setEncrypt(false);
-                    return noteForm;
-                 })
+            // set all note encrypt to false
+            List<NoteForm> noteFormList = noteService.findAllByUsername(curUser)
+                    .stream()
+                    .map((noteDO) -> {
+                        NoteForm noteForm = NoteDOMapper.INSTANCE.noteDOToNoteForm(noteDO);
+                        noteForm.setEncrypt(false);
+                        return noteForm;
+                    })
 
-                 .collect(Collectors.toList());
+                    .collect(Collectors.toList());
 
-         for (NoteForm noteForm : noteFormList) {
-            noteService.updateNote(curUser, noteForm);
-         }
-      } else {
-         userDO.setNotePassword(notePassword);
-      }
-      userMapper.updateById(userDO);
+            for (NoteForm noteForm : noteFormList) {
+                noteService.updateNote(curUser, noteForm);
+            }
+        } else {
+            userDO.setNotePassword(notePassword);
+        }
+        userMapper.updateById(userDO);
 
-      return ResponseVO.success();
-   }
+        return ResponseVO.success();
+    }
 
-   @PostMapping("/validateNotePassword/{notePassword}")
-   private ResponseVO validateNotePassword(@PathVariable String notePassword, @RequestHeader HttpHeaders headers) {
-      String curUser = tokenService.getUsername(headers.getFirst("token"));
+    @PostMapping("/validateNotePassword/{notePassword}")
+    private ResponseVO validateNotePassword(@PathVariable String notePassword, @RequestHeader HttpHeaders headers) {
+        String curUser = tokenService.getUsername(headers.getFirst("token"));
 
-      UserDO userDO = userService.findUserDO(curUser);
-      if (!userDO.getNotePassword().equals(notePassword)) {
-         throw new RuntimeException("note password is wrong");
-      }
-      return ResponseVO.success("note password is validated");
-   }
+        UserDO userDO = userService.findUserDO(curUser);
+        if (!userDO.getNotePassword().equals(notePassword)) {
+            throw new RuntimeException("note password is wrong");
+        }
+        return ResponseVO.success("note password is validated");
+    }
 
-   @PutMapping("/changePassword")
-   private ResponseVO<UserDO> changePassword(@Valid @RequestBody ChangePasswordForm form) {
-      UserDO userDO = userService.changePassword(form);
-      return ResponseVO.success(userDO);
-   }
+    @PutMapping("/changePassword")
+    private ResponseVO<UserDO> changePassword(@Valid @RequestBody ChangePasswordForm form) {
+        UserDO userDO = userService.changePassword(form);
+        return ResponseVO.success(userDO);
+    }
 
-   @DeleteMapping
-   private ResponseVO delete(@RequestBody LoginForm form) {
-      userService.delete(form);
+    @DeleteMapping
+    private ResponseVO delete(@RequestBody LoginForm form) {
+        userService.delete(form);
 
-      return ResponseVO.success("删除成功");
-   }
+        return ResponseVO.success("删除成功");
+    }
 }
